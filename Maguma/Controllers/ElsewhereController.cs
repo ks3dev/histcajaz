38b447ac54e7d74f45e04ea0ca4691826eb94abd
Nick Josevski <nickjosevski@gmail.com>
2012-03-08 22:37:30 +1100

    added another shell controller, some css changes, and now history.js can be seen in action

---
+++ Maguma/Controllers/ElsewhereController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Maguma.Controllers
+{
+    public class ElsewhereController : Controller
+    {
+        //
+        // GET: /Elsewhere/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
