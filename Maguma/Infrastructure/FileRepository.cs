8eb723cb706182d06ba589620d5296a0d5379d73
Nick Josevski <nickjosevski@gmail.com>
2012-03-08 20:54:18 +1100

    restructured to a Public folder, moving css, and scripts

---
+++ Maguma/Infrastructure/FileRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Maguma.Infrastructure
+{
+    public class FileRepository
+    {
+        //Can remove this needed a class in the folder so it becomes part of the git repo
+    }
+}
---
+++ Maguma/Model/SystemUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Maguma.Model
+{
+    public class SystemUser
+    {
+        //Can remove this needed a class in the folder so it becomes part of the git repo
+    }
+}
