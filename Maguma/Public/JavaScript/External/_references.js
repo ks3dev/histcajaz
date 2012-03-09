195922d701949a38e88225648b91c92ab1c57ee4
Nick Josevski <NickJosevski@gmail.com>
2012-03-09 14:52:45 +1100

    removed files no longer needed

--- Maguma/Public/JavaScript/External/_references.js
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="jquery-1.7.1.js" />
-/// <reference path="jquery-ui-1.8.18.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="modernizr-2.5.3.js" />
--- Maguma/Public/JavaScript/_references.js
+++
@@ -1,1 +0,0 @@
-﻿/// <reference path="History.Setup.js" />
